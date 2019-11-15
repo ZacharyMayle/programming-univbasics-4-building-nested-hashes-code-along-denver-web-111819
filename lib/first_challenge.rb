16081946a56f7a251f451ff4d02c3533d12d7fee
Zachary Mayle <zam1998@gmail.com>
2019-11-15 21:01:29 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,21 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {:name => "Lord Montague", :age => "53"},
+       :matriarch => {:name => "Lady Montague", :age => "54"},
+       :hero => {:name => "Romeo", :aghe => "15", :status => "alive"},
+       :hero_friends => [name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+     }
+     :capulet => {
+       :patriarch => {:name => "Lord Capulet"},
+       :matriarch => {:name => "Lady Capulet"},
+       :heroine {:name => "Juliet", :age => "15", :status => "alive"},
+       :heroine_friends => [
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+     }
+    
+   }
 end
