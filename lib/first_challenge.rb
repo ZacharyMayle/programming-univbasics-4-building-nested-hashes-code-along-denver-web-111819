e9553be13af8399cb361498fc9b28613e4de0f8f
Zachary Mayle <zam1998@gmail.com>
2019-11-15 20:47:53 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,17 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {:name => "Lord Montague", :age => "53"},
+       :matriarch => {:name => "Lady Montague", :age => "54"},
+       :hero => {:name => "Romeo", :aghe => "15", :status => "alive"},
+       :hero_friends => {}
+     }
+     :capulet => {
+       :patriarch => {:name => "Lord Capulet"},
+       :matriarch => {:name => "Lady Capulet"},
+       :heroine {:name => "Juliet", :age => "15", :status => "alive"},
+       :heroine_friends => {}
+     }
+    
+   }
 end
