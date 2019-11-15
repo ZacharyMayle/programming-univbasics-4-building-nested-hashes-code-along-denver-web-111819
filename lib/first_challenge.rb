5a0044482ec04708f9a59c1a5baba055d2f75d8c
Zachary Mayle <zam1998@gmail.com>
2019-11-15 20:53:26 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,13 +4,13 @@ def first_challenge
        :patriarch => {:name => "Lord Montague", :age => "53"},
        :matriarch => {:name => "Lady Montague", :age => "54"},
        :hero => {:name => "Romeo", :aghe => "15", :status => "alive"},
-       :hero_friends => {}
+       :hero_friends => [ benvolio = {:name => "Benvolio", 
      }
      :capulet => {
        :patriarch => {:name => "Lord Capulet"},
        :matriarch => {:name => "Lady Capulet"},
        :heroine {:name => "Juliet", :age => "15", :status => "alive"},
-       :heroine_friends => {}
+       :heroine_friends => []
      }
     
    }
